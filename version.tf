9e5ca052e360169cfde9c04f86780879b14b680e
pradeepbhadani <pradeep.bhadani@gmail.com>
2019-03-13 11:09:38 +0000

    Pin module to use 1.60.0 version of aws provider (#9)
    * Pin module to use 1.60.0 version of aws provider
    
    * update version condition

---
+++ version.tf
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2018 Expedia Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+
+terraform {
+  required_version = "~> 0.11.1"
+}
+
+provider "aws" {
+  version = "~> 1.60.0"
+}
